1eaf85e3aa2edc77a2ff35b4f4f7dc13c53baa76
Til Freitag <104679877+Til2ktyp@users.noreply.github.com>
2026-04-25 20:04:47 +0200

    changed touch things

--- Aktienrechner/assets-beta/since-beta-ui.js
+++ Aktienrechner/assets-beta/since-beta-ui.js
@@ -328,3 +328,15 @@
         });
 
         window.addEventListener('resize', updateZoomControls);
+
+        let lastTouchEnd = 0;
+
+        document.addEventListener('touchend', function(event) {
+            const now = Date.now();
+
+            if (now - lastTouchEnd <= 300) {
+                event.preventDefault();
+            }
+
+            lastTouchEnd = now;
+        }, { passive: false });
